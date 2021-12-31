e8da423d7e3e05edba0cff1033b2ba3ed8173463
Maël <mael@puremix.net>
2021-12-31 19:09:18 +0100

    Add target: 'static' in Nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,6 @@ export default {
         href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@500&family=Poppins:wght@500&display=swap'
       }
     ]
-
-  }
+  },
+  target: 'static'
 }
